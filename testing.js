2796839532634f2a61eb1704856282fe1dcb85c5
Michael Krautkramer <michaelkrauty@gmail.com>
2018-12-18 13:48:06 -0800

    use tabs instead of spaces

--- testing.js
+++ testing.js
@@ -6,18 +6,18 @@ const port = 80;
 var con;
 
 con = mysql.createConnection({
-    host: config['db']['host'],
-    user: config['db']['user'],
-    database: config['db']['database'],
-    password: config['db']['password'],
+	host: config['db']['host'],
+	user: config['db']['user'],
+	database: config['db']['database'],
+	password: config['db']['password'],
 });
 con.connect(function (err) {
-    if (err) throw err;
-    console.log('Connected to MySQL DB!');
+	if (err) throw err;
+	console.log('Connected to MySQL DB!');
 });
 var sql = 'SELECT * FROM users';
 con.query(sql, function (err, res) {
-    if (err) throw err;
-    console.log(res);
-    con.end();
+	if (err) throw err;
+	console.log(res);
+	con.end();
 });
