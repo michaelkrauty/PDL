53ce5bd82c5eb3f5035e30644a403b6f124fb21a
Michael Krautkramer <michaelkrauty@gmail.com>
2018-12-18 14:27:36 -0800

    move DB functions to DB.js

---
+++ DB.js
@@ -0,0 +1,123 @@
+const config = require('./config.js');
+const mysql = require('mysql');
+const log = require('winston');
+var con;
+
+exports.connect = function () {
+	// connect to MySQL DB
+	log.info('Connecting to MySQL DB: ' + config['db']['user'] + '@' + config['db']['host'] + '...');
+	con = mysql.createConnection({
+		host: config['db']['host'],
+		user: config['db']['user'],
+		password: config['db']['password'],
+	});
+	con.connect(function (err) {
+		if (err) throw err;
+		log.info('Connected to MySQL DB!');
+	});
+	// create DB if it doesn't already exist
+	con.query('CREATE DATABASE IF NOT EXISTS `' + config['db']['database'] + '`', function (err) {
+		if (err) throw err;
+	});
+	// select DB
+	con.query('USE `' + config['db']['database'] + '`', function (err) {
+		if (err) throw err;
+	});
+	// create DB tables if they don't already exist
+	con.query('CREATE TABLE IF NOT EXISTS users (id bigint primary key auto_increment, discord_username varchar(255), discord_id bigint, 1v1_elo int not null default 1500, competing boolean not null default 0)', function (err, res) {
+		if (err) throw err;
+		if (res['warningCount'] == 0)
+			log.info('Created MySQL table "users"');
+	});
+	con.query('CREATE TABLE IF NOT EXISTS quests (id bigint primary key auto_increment, player_id bigint, quest varchar(255), amount int)', function (err, res) {
+		if (err) throw err;
+		if (res['warningCount'] == 0)
+			log.info('Created MySQL table "quests"');
+	});
+}
+
+// check if user exists in DB
+// returns {success: boolean, exists: boolean}
+exports.checkUserExists = function (discord_id) {
+	return new Promise(async function (resolve, reject) {
+		var sql = 'SELECT id FROM users WHERE discord_id=?';
+		await con.query(sql, discord_id, function (err, res) {
+			if (err) throw err;
+			if (res.length > 0) {
+				resolve({ success: true, exists: true });
+			} else {
+				resolve({ success: true, exists: false });
+			}
+		});
+	});
+}
+
+// register new user
+// returns {success: boolean}
+exports.registerUser = function (discord_id, discord_username) {
+	return new Promise(async function (resolve, reject) {
+		exports.checkUserExists(discord_id).then(function (value) {
+			if (value['success']) {
+				if (value['exists']) {
+					resolve({ success: false });
+				} else {
+					exports.createUserInDB(discord_id, discord_username).then(function (value) {
+						resolve({ success: value['success'] });
+					});
+				}
+			}
+		});
+	});
+}
+
+// create new user in DB
+// returns {success: boolean}
+exports.createUserInDB = function (discord_id, discord_username) {
+	return new Promise(async function (resolve, reject) {
+		var sql = 'INSERT INTO users (discord_username, discord_id) VALUES (?,?)';
+		await con.query(sql, [discord_username, discord_id], function (err) {
+			if (err) throw err;
+			resolve({ success: true });
+		});
+	});
+}
+
+// check if user is competing
+// returns {success: boolean, competing: boolean}
+exports.isUserCompeting = function (discord_id) {
+	return new Promise(async function (resolve, reject) {
+		var sql = 'SELECT competing FROM users WHERE discord_id=?';
+		await con.query(sql, discord_id, function (err, res) {
+			if (err) throw err;
+			if (res.length > 0) {
+				resolve({ success: true, competing: res[0]['competing'] });
+			}
+		});
+	});
+}
+
+// set user's competing boolean
+// returns {success: boolean}
+exports.setUserCompeting = function (discord_id, competing) {
+	return new Promise(async function (resolve, reject) {
+		var sql = 'UPDATE users SET competing=? WHERE discord_id=?';
+		await con.query(sql, [competing, discord_id], function (err) {
+			if (err) throw err;
+			resolve({ success: true });
+		});
+	});
+}
+
+// get user's ELO
+// returns {success: boolean, elo: int}
+exports.getELO = function (discord_id) {
+	return new Promise(async function (resolve, reject) {
+		var sql = 'SELECT 1v1_elo FROM users WHERE discord_id=?';
+		await con.query(sql, discord_id, function (err, res) {
+			if (err) throw err;
+			if (res.length > 0) {
+				resolve({ success: true, elo: res[0]['1v1_elo'] });
+			}
+		});
+	});
+}
--- PDLbot.js
+++ PDLbot.js
@@ -1,9 +1,7 @@
-var discord = require('discord.io');
-var log = require('winston');
-var mysql = require('mysql');
-var auth = require('./auth.json');
-var config = require('./config.js');
-var con;
+const discord = require('discord.io');
+const log = require('winston');
+const auth = require('./auth.json');
+const db = require('./DB.js');
 // configure logger settings
 log.remove(log.transports.Console);
 log.add(new log.transports.Console, {
@@ -18,36 +16,7 @@ var bot = new discord.Client({
 // bot startup
 bot.on('ready', function (evt) {
 	log.info('Logged in as: ' + bot.username + ' - (' + bot.id + ')');
-	log.info('Connecting to MySQL DB: ' + config['db']['user'] + '@' + config['db']['host'] + '...');
-	// connect to MySQL DB
-	con = mysql.createConnection({
-		host: config['db']['host'],
-		user: config['db']['user'],
-		password: config['db']['password'],
-	});
-	con.connect(function (err) {
-		if (err) throw err;
-		log.info('Connected to MySQL DB!');
-	});
-	// create DB if it doesn't already exist
-	con.query('CREATE DATABASE IF NOT EXISTS `' + config['db']['database'] + '`', function (err) {
-		if (err) throw err;
-	});
-	// select DB
-	con.query('USE `' + config['db']['database'] + '`', function (err) {
-		if (err) throw err;
-	});
-	// create DB tables if they don't already exist
-	con.query('CREATE TABLE IF NOT EXISTS users (id bigint primary key auto_increment, discord_username varchar(255), discord_id bigint, 1v1_elo int not null default 1500, competing boolean not null default 0)', function (err, res) {
-		if (err) throw err;
-		if (res['warningCount'] == 0)
-			log.info('Created MySQL table "users"');
-	});
-	con.query('CREATE TABLE IF NOT EXISTS quests (id bigint primary key auto_increment, player_id bigint, quest varchar(255), amount int)', function (err, res) {
-		if (err) throw err;
-		if (res['warningCount'] == 0)
-			log.info('Created MySQL table "quests"');
-	});
+	db.connect();
 });
 
 // called when the bot sees a message
@@ -65,7 +34,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				break;
 			case 'register':
 				// register new user
-				registerUser(userID, user).then(function (value) {
+				db.registerUser(userID, user).then(function (value) {
 					if (value['success']) {
 						botMessage(channelID, tag(userID) + ' is now registered in the Pavlov Duel League!');
 					} else {
@@ -75,8 +44,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				break;
 			case 'compete':
 				// register user if they're not already in the DB
-				registerUser(userID, user).then(function () {
-					setUserCompeting(userID, true).then(function (value) {
+				db.registerUser(userID, user).then(function () {
+					db.setUserCompeting(userID, true).then(function (value) {
 						if (value['success']) {
 							botMessage(channelID, tag(userID) + ' is now competing in PDL!');
 						}
@@ -85,9 +54,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				break;
 			case 'retire':
 				// stop competing, but keep data in DB
-				checkUserExists(userID).then(function (value) {
+				db.checkUserExists(userID).then(function (value) {
 					if (value['success'] && value['exists']) {
-						setUserCompeting(userID, false).then(function (value) {
+						db.setUserCompeting(userID, false).then(function (value) {
 							if (value['success']) {
 								botMessage(channelID, tag(userID) + ' is no longer competing in PDL!');
 							}
@@ -99,9 +68,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				break;
 			case 'competing':
 				// check if user is currently competing
-				checkUserExists(userID).then(function (value) {
+				db.checkUserExists(userID).then(function (value) {
 					if (value['success'] && value['exists']) {
-						isUserCompeting(userID).then(function (value) {
+						db.isUserCompeting(userID).then(function (value) {
 							if (value['success'] && value['competing']) {
 								botMessage(channelID, tag(userID) + ' is competing.');
 							} else {
@@ -115,7 +84,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				break;
 			case 'check':
 				// check if user exists in DB
-				checkUserExists(userID).then(function (value) {
+				db.checkUserExists(userID).then(function (value) {
 					if (value['success'] && value['exists']) {
 						botMessage(channelID, tag(userID) + ' is registered in the Pavlov Duel League.');
 					} else {
@@ -125,9 +94,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				break;
 			case 'elo':
 				// get user ELO
-				checkUserExists(userID).then(function (value) {
+				db.checkUserExists(userID).then(function (value) {
 					if (value['success'] && value['exists']) {
-						getELO(userID).then(function (value) {
+						db.getELO(userID).then(function (value) {
 							if (value['success']) {
 								botMessage(channelID, tag(userID) + ' your ELO is ' + value['elo'] + '.');
 							} else {
@@ -143,92 +112,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		}
 	}
 });
-
-// check if user exists in DB
-// returns {success: boolean, exists: boolean}
-function checkUserExists(discord_id) {
-	return new Promise(async function (resolve, reject) {
-		var sql = 'SELECT id FROM users WHERE discord_id=?';
-		await con.query(sql, discord_id, function (err, res) {
-			if (err) throw err;
-			if (res.length > 0) {
-				resolve({ success: true, exists: true });
-			} else {
-				resolve({ success: true, exists: false });
-			}
-		});
-	});
-}
-
-// register new user
-// returns {success: boolean}
-function registerUser(discord_id, discord_username) {
-	return new Promise(async function (resolve, reject) {
-		checkUserExists(discord_id).then(function (value) {
-			if (value['success']) {
-				if (value['exists']) {
-					resolve({ success: false });
-				} else {
-					createUserInDB(discord_id, discord_username).then(function (value) {
-						resolve({ success: value['success'] });
-					});
-				}
-			}
-		});
-	});
-}
-
-// create new user in DB
-// returns {success: boolean}
-function createUserInDB(discord_id, discord_username) {
-	return new Promise(async function (resolve, reject) {
-		var sql = 'INSERT INTO users (discord_username, discord_id) VALUES (?,?)';
-		await con.query(sql, [discord_username, discord_id], function (err) {
-			if (err) throw err;
-			resolve({ success: true });
-		});
-	});
-}
-
-// check if user is competing
-// returns {success: boolean, competing: boolean}
-function isUserCompeting(discord_id) {
-	return new Promise(async function (resolve, reject) {
-		var sql = 'SELECT competing FROM users WHERE discord_id=?';
-		await con.query(sql, discord_id, function (err, res) {
-			if (err) throw err;
-			if (res.length > 0) {
-				resolve({ success: true, competing: res[0]['competing'] });
-			}
-		});
-	});
-}
-
-// set user's competing boolean
-// returns {success: boolean}
-function setUserCompeting(discord_id, competing) {
-	return new Promise(async function (resolve, reject) {
-		var sql = 'UPDATE users SET competing=? WHERE discord_id=?';
-		await con.query(sql, [competing, discord_id], function (err) {
-			if (err) throw err;
-			resolve({ success: true });
-		});
-	});
-}
-
-// get user's ELO
-// returns {success: boolean, elo: int}
-function getELO(discord_id) {
-	return new Promise(async function (resolve, reject) {
-		var sql = 'SELECT 1v1_elo FROM users WHERE discord_id=?';
-		await con.query(sql, discord_id, function (err, res) {
-			if (err) throw err;
-			if (res.length > 0) {
-				resolve({ success: true, elo: res[0]['1v1_elo'] });
-			}
-		});
-	});
-}
 
 // send a message from the bot
 function botMessage(channelID, message) {
