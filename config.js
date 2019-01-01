bbf60b95a59234ebfa306f11369ff91560594ba6
Michael Krautkramer <michaelkrauty@gmail.com>
2019-01-01 02:20:05 -0800

    update config

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
 module.exports.config = {
+	// rating method (0 = elo, 1 = glicko2 live, 2 = glicko2 schedule)
 	rating_method: 0,
-	elo_k: 50
+	// elo K variable
+	elo_k: 50,
+	// weekly limit on challenges submitted per user
+	maximum_weekly_challenges: 3,
+	// bonus elo per game simply for playing
+	bonus_elo: 5,
+	admin_discord_ids: []
 };
